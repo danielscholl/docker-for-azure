d0e1aea3dfbad8226a08679c6b694c01243d9865
Daniel Scholl <daniel@scholllopez.com>
2018-10-16 12:26:37 -0500

    Fixed install script

--- install.sh
+++ install.sh
@@ -130,7 +130,7 @@ tput setaf 2; echo 'Deploying ARM Template...' ; tput sgr0
 if [ -f ./params.json ]; then PARAMS="params.json"; else PARAMS="azuredeploy.parameters.json"; fi
 az deployment create --template-file azuredeploy.json  \
     --name "$INITIALS-swarm" \
-    --prefix $INITIALS \
+    --initials $INITIALS \
     --location $AZURE_LOCATION \
     --parameters $PARAMS \
     --parameters random=$UNIQUE \
